21c9908f788912f752076b4e575bad700841d7d5
YERIN <dlyefls123@naver.com>
2025-03-23 19:25:34 +0900

    과제하자자

--- inputTest.py
+++ inputTest.py
@@ -1,9 +1,20 @@
-num1 = input("숫자1 ==>")
-num1 = int(num1)
-print(num1)
-num2 = input("숫자2 ==>")
-num2 = int(num2)
-print(num2)
+num1 = int(input("숫자1 ==>"))
+num2 = int(input("숫자2 ==>"))
 
-numSum = num1 + num2
-print(numSum)
+result1 = num1 + num2
+print(num1, "+", num2, "=", result1)
+
+result2 = num1 - num2
+print(num1,"-",num2,"=",result2)
+
+result3 = num1 * num2
+print(num1,"*",num2,"=",result3)
+
+result4 = num1 / num2
+print(num1,"/",num2,"=",result4)
+
+result5 = num1 % num2
+print(num1,"%",num2,"=",result5)
+
+result6 = num1 ** num2
+print(num1,"**",num2,"=",result6)
