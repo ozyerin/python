b45e4c63b17b95a408007d3014e9cff3c92336f0
ddd <dlyefls123@naver.com>
2025-05-12 11:30:38 +0900

    ㅠㅠ

--- 수업/0512.py
+++ 수업/0512.py
@@ -1,6 +1,9 @@
 #read.py로 파일 이름 설정하심....
 #왜 텍스트 파일을 못읽을까..^^!~~~
 
+
+#파일 읽는거
+
 '''
 file = open("test.txt","r",encoding="UTF-8")
 
@@ -36,20 +39,50 @@ for str1 in fileList :
 
 file.close()
 
-'''
-
 import tkinter
 
 root = tkinter.Tk()
 
-file = open("test.txt","r",encoding="UTF-8")
-
-strFile = file.readline()
-root.title(strFile[:-1])
+file = open("C:/Users/user/Desktop/python/수업/test.txt","r",encoding="UTF-8")
 
 strFile = file.readline()
 root.geometry(strFile[:-1])
 
+strFile = file.readline()
+root.title(strFile[:-1])
+
 file.close()
 
 root.mainloop()
+
+
+#파일 이름 write.py로 하심용 이번엔 쓰기 입니더...
+
+outFile = open("C:/Users/user/Desktop/python/수업/outTest.txt","w",encoding="UTF-8")
+
+while True :
+    outStr = input("내용입력==>")
+    #사용자가 끝이라 입력하면 종료
+    if outStr == '끝':
+        break
+    outFile.writelines(outStr+"\n")
+
+
+outFile.close()
+
+'''
+
+#copyCopy.py == > test파일을 읽어와서 outtest파일에 적용
+
+
+inFile = open("C:/Users/user/Desktop/python/수업/test.txt","r",encoding="UTF-8")
+outFile = open("C:/Users/user/Desktop/python/수업/outTest.txt","w",encoding="UTF-8")
+
+while True:
+    strFile = inFile.readline()
+    if(str==""):
+        break
+    outFile.writelines(strFile)
+
+inFile.close()
+outFile.close()
